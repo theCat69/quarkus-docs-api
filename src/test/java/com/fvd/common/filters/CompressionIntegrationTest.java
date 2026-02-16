dff4cba28007abe72056a7e4582fb2ae9ee19885
Felix Vadcard <vadcard.felix@gmail.com>
2026-02-16 17:13:05 +0100

    1_3_0 / ai / implementing: feature-90-http-response-compression.md done. Enabling gzip compression for application/json and text/plain responses via application.properties config. Includes integration tests verifying Content-Encoding: gzip header.

---
+++ src/test/java/com/fvd/common/filters/CompressionIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.fvd.common.filters;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.notNullValue;
+
+@QuarkusTest
+class CompressionIntegrationTest {
+
+    @Test
+    void testGzipCompressionEnabled() {
+        given()
+                .header("Accept-Encoding", "gzip")
+                .queryParam("keywords", "security")
+        .when()
+                .get("/api/search")
+        .then()
+                .statusCode(200)
+                .header("Content-Encoding", containsString("gzip"));
+    }
+
+    @Test
+    void testCompressedResponseIsValidJson() {
+        given()
+                .queryParam("keywords", "security")
+        .when()
+                .get("/api/search")
+        .then()
+                .statusCode(200)
+                .body("results", notNullValue());
+    }
+}
